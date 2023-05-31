decb2840a7632871a83afc5bb8e6aad214644ab1
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-05-31 16:05:31 +0530

    added domain on cookie

--- controller/fileUploadController.js
+++ controller/fileUploadController.js
@@ -29,7 +29,7 @@ exports.setCookie = async (req, res, next) => {
       secure: true,
       expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
       path: "/",
-      domain: "file-uploader-server-mern.vercel.app",
+      domain: "file-uploader-client.vercel.app",
       sameSite: "none",
     })
     .json({
