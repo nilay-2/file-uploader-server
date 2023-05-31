569b18a71bb3edb7c32d0ced9bdda4a355bf6df1
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-05-31 14:56:51 +0530

    added cookie-parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const globalErrHandler = require("./globalErrHandler");
 const fileUploadRouter = require("./routes/fileUploadRoute");
 dotenv.config({ path: "./.env" });
@@ -10,6 +11,7 @@ app.use(
     origin: "https://file-uploader-client.vercel.app",
   })
 );
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.static("public"));
 const port = process.env.PORT || 8000;
